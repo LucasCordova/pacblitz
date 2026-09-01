eb2a0e8fdf33a62852209540be67b7fb935cb4f6
Lucas Cordova <lpcordova@outlook.com>
2026-09-01 08:50:13 -0700

    W2T1 demo: wiretap both clocks; commented time_scale experiment

--- main.gd
+++ main.gd
@@ -5,6 +5,8 @@ const BERRY := preload("res://berry.tscn")
 var berries: int = 0
 
 func _ready() -> void:
+    # Demo 3 - bend time: uncomment, run, and enjoy global slow motion.
+    # Engine.time_scale = 0.3
     for i in range(12):
         var berry := BERRY.instantiate()
         berry.position = Vector2(120 + i * 80, 324)
